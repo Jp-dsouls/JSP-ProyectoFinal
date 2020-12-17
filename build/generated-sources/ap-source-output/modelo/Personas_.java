c01573371f7ebb53997f493ad83cf20bf1e167d4
Jp-dsouls <werras04@gmail.com>
2020-12-17 17:10:40 -0500

    Sugerencias
    En la pestaña de registrate cuando se le hace click a pagar ahora creo que debemos poner las listas que estan en index ahora yo veo eso mas rato
    Para que te ubiques esta comentado el formulario de esa pg

---
+++ build/generated-sources/ap-source-output/modelo/Personas_.java
@@ -0,0 +1,15 @@
+package modelo;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-12-17T16:34:30")
+@StaticMetamodel(Personas.class)
+public class Personas_ { 
+
+    public static volatile SingularAttribute<Personas, String> conper;
+    public static volatile SingularAttribute<Personas, String> nomper;
+    public static volatile SingularAttribute<Personas, Integer> codper;
+
+}
